bb9c65f6d3923cb1c1be1f451bc12de348cf9713
Andrei Popovici <me@andreipopovici.net>
2013-08-15 05:30:16 +0000

    =Cleanup

--- lib/getty_up/api/util.rb
+++ lib/getty_up/api/util.rb
@@ -4,23 +4,20 @@ module GettyUp
   module API
     module Util
       def post_json(request, endpoint, debug = false)
-        if debug
-          puts  '--- Request to Getty --'
-          puts  request
-        end
+      
+       if debug
+         puts  '--- Request to Getty --'
+         puts  request
+       end
 
-        #You may wish to replace this code with your preferred library for posting and receiving JSON data.
-#        uri = URI.parse(endpoint)
-#       http = Net::HTTP.new(uri.host, 443)
-#        http.use_ssl = true
+       response = RestClient.post endpoint, request.to_json, :content_type => :json, :accept => :json
 
-#        response = http.post(uri.path, request.to_json, {'Content-Type' =>'application/json'}).body
-        response = RestClient.post endpoint, request.to_json, :content_type => :json, :accept => :json
-        if debug
+       if debug
           puts  '--- Response from Getty --'
           puts  response
-        end
-        JSON.parse(response)
+       end
+      
+       JSON.parse(response)
 
       end
 
